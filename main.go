71a7d0c2e39eceb647884afd7ce411978821fe35
DevSirq <joaosirqueirasantos@gmail.com>
2025-05-07 17:19:11 -0300

    Usando GIN Framework, criando endpoint

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
-import "fmt"
+import (
+	 "github.com/gin-gonic/gin"
+)
+
+// type Pizza struct {
+// 	ID int
+// 	nome string
+// 	preco float64
+// }
 
 func main() {
-	var nomePizzaria string = "Pizzaria Go"
-	instagram, telefone := "@pizzaria_go", 11951
-	fmt.Println(nomePizzaria, instagram, telefone)
+	router := gin.Default()
+	// Criando uma rota
+	router.GET("/pizzas", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"pizzas": "Toscana, Calabresa",
+		})
+	})
+	
+	// var pizzas = []Pizza{
+	// 	{ID: 1, nome: "Toscana", preco: 49.5},
+	// 	{ID: 2, nome: "Marguerita", preco: 79.5},
+	// 	{ID: 3, nome: "Atum com Queijo", preco: 69.5},
+	// }
+	// fmt.Println(pizzas)
+	router.Run() // Executar rota
 }
 
-// executar a operação:
-// go run . 
+// executar a operação: go run . 
+// trazer dependências e pacotes externos: go get
 
+// dependência do gin:
+// go get github.com/gin-gonic/gin
