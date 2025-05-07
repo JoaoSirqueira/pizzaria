63c7cec4e430dd7fed4cb169eaac1c80ce6b83b9
DevSirq <joaosirqueirasantos@gmail.com>
2025-05-07 17:47:56 -0300

    Agora a API mostra o ID, Nome e Preço das pizzas (Criação do endpoint e exportação e acessos)

--- main.go
+++ main.go
@@ -3,33 +3,44 @@ package main
 import (
 	 "github.com/gin-gonic/gin"
 )
-
-// type Pizza struct {
-// 	ID int
-// 	nome string
-// 	preco float64
-// }
+type Pizza struct {
+	ID int `json:"id"`
+	Nome string `json:"nome"`
+	Preco float64 `json:"preco"`
+}
 
 func main() {
 	router := gin.Default()
 	// Criando uma rota
-	router.GET("/pizzas", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"pizzas": "Toscana, Calabresa",
-		})
-	})
+	router.GET("/pizzas", getPizzas)
 	
-	// var pizzas = []Pizza{
-	// 	{ID: 1, nome: "Toscana", preco: 49.5},
-	// 	{ID: 2, nome: "Marguerita", preco: 79.5},
-	// 	{ID: 3, nome: "Atum com Queijo", preco: 69.5},
-	// }
-	// fmt.Println(pizzas)
 	router.Run() // Executar rota
 }
 
-// executar a operação: go run . 
+func getPizzas(c *gin.Context) {
+	var pizzas = []Pizza{
+		{ID: 1, Nome: "Toscana", Preco: 49.5},
+		{ID: 2, Nome: "Marguerita", Preco: 79.5},
+		{ID: 3, Nome: "Atum com Queijo", Preco: 69.5},
+	}
+
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
+}
+
+
+// executar a operação: go run . OU go run main.go
 // trazer dependências e pacotes externos: go get
 
 // dependência do gin:
 // go get github.com/gin-gonic/gin
+
+// utilizar atributos de uma struct com letra minúscula significa que eles são privados, ou seja, não pode acessar essas informações de outros lugares
+// utilizar atributos de uma struct com a primeira letra maiúscula significa que eles são públicos, ou seja, pode ser acessados essas informações de outros lugares.
+// Quando trabalhamos com API é comum deixar as coisas com minúsculos para fazer com o GO usa-se um highlight e coloca o código: `json:"id"` exemplo:
+// type Pizza struct {
+//     ID    int     `json:"id"`
+//     Nome  string  `json:"nome"`
+//     Preco float64 `json:"preco"`
+// }
