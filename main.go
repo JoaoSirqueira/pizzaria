ecda91e9c3472e11522fd1756b392f9acbabc379
DevSirq <joaosirqueirasantos@gmail.com>
2025-05-07 18:29:01 -0300

    Modularizando as aplicações e criado a pasta models

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	 "github.com/gin-gonic/gin"
+	"pizzaria/models"
+	"github.com/gin-gonic/gin"
 )
-type Pizza struct {
-	ID int `json:"id"`
-	Nome string `json:"nome"`
-	Preco float64 `json:"preco"`
-}
 
 func main() {
 	router := gin.Default()
@@ -18,7 +14,7 @@ func main() {
 }
 
 func getPizzas(c *gin.Context) {
-	var pizzas = []Pizza{
+	var pizzas = []models.Pizza{
 		{ID: 1, Nome: "Toscana", Preco: 49.5},
 		{ID: 2, Nome: "Marguerita", Preco: 79.5},
 		{ID: 3, Nome: "Atum com Queijo", Preco: 69.5},
